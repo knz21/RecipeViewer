9d378f9fa4ac464b20854522532340f5d1dd4ecd
knz21 <cismollco@gmail.com>
2017-09-29 15:10:51 +0900

    create project

---
+++ app/src/main/kotlin/com/knz21/recipeviewer/RecipeActivity.kt
@@ -0,0 +1,12 @@
+package com.knz21.recipeviewer
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class RecipeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe)
+    }
+}
