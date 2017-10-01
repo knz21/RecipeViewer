219c68946497d166b0b0cf07fda4702f150cc321
knz21 <cismollco@gmail.com>
2017-10-01 18:45:25 +0900

    fix layout

--- app/src/main/kotlin/com/knz21/android/recipeviewer/presentation/RecipeActivity.kt
+++ app/src/main/kotlin/com/knz21/android/recipeviewer/presentation/RecipeActivity.kt
@@ -26,7 +26,9 @@ class RecipeActivity : AppCompatActivity(), RecipePresenter.Contract {
         super.onCreate(savedInstanceState)
         (application as RecipeApp).component.plus(RecipeModule(this)).inject(this)
         binding.listView.layoutManager = GridLayoutManager(this, 2)
-        binding.listView.addItemDecoration(RecipeListDecoration())
+        binding.listView.addItemDecoration(RecipeListDecoration(this))
+        binding.bottomBar.setOnTabSelectListener { Timber.i("tabselecterd") }
+        binding.bottomBar.setOnTabReselectListener { binding.listView.smoothScrollToPosition(0) }
         presenter.getRecipes()
     }
 
@@ -57,13 +59,16 @@ class RecipeActivity : AppCompatActivity(), RecipePresenter.Contract {
 
     private data class RecipeItemViewHolder(val binding: RecipeItemBinding) : RecyclerView.ViewHolder(binding.root)
 
-    private class RecipeListDecoration : RecyclerView.ItemDecoration() {
-        companion object {
-            private const val OFFSET = 10
-        }
+    private class RecipeListDecoration(context: Context) : RecyclerView.ItemDecoration() {
+        private val offset: Int by lazy { context.resources.getDimensionPixelSize(R.dimen.cell_offset) }
 
         override fun getItemOffsets(outRect: Rect?, view: View?, parent: RecyclerView?, state: RecyclerView.State?) {
-            outRect?.set(OFFSET, OFFSET, OFFSET, OFFSET)
+            outRect?.run {
+                left = offset
+                parent?.getChildAdapterPosition(view)?.let { top = if (it < 2) offset * 2 else 0 }
+                right = offset
+                bottom = offset * 2
+            }
         }
     }
 }
